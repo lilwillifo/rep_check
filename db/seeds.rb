d5cb98922351efacc2ccc4d185dc963b5dc59868
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-29 18:57:32 -0600

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,26 @@ response[:votes].each do |vote|
               republican_majority_position: vote[:republican][:majority_position]
               )
           end
+
+rep1 = service.get_url("/congress/v1/members/house/CO/1/current.json").first[:name]
+rep2 = service.get_url("/congress/v1/members/house/CO/2/current.json").first[:name]
+rep3 = service.get_url("/congress/v1/members/house/CO/3/current.json").first[:name]
+rep4 = service.get_url("/congress/v1/members/house/CO/4/current.json").first[:name]
+rep5 = service.get_url("/congress/v1/members/house/CO/5/current.json").first[:name]
+rep6 = service.get_url("/congress/v1/members/house/CO/6/current.json").first[:name]
+rep7 = service.get_url("/congress/v1/members/house/CO/7/current.json").first[:name]
+reps = [rep1, rep2, rep3, rep4, rep5, rep6, rep7]
+
+
+def vote(roll_call, name)
+  response = get_url("/congress/v1/115/house/sessions/1/votes/#{@roll_call}.json")
+  positions = response[:votes][:vote][:positions]
+  raw_rep_position = positions.select {|member| member[:name] == name}
+  vote_position = raw_rep_position.first[:vote_position]
+end
+
+reps.each do |rep_name|
+  Bill.all.each do |bill|
+    RepVotes.create(bill_id: bill.id, rep_name: rep_name, vote_with: vote(bill.roll_call, rep_name) )
+  end
+end
