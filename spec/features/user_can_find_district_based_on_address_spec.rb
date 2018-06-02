8c0cbd4755b99a90d2ca83d84ebd5a63b198fc8c
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-02 15:03:55 -0600

    user can enter address and be routed to their district page'

--- spec/features/user_can_find_district_based_on_address_spec.rb
+++ spec/features/user_can_find_district_based_on_address_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe 'As a user on the root page' do
   it 'I can enter my address and find my congressional district' do
     VCR.use_cassette('search by address') do
+      Representative.create(name: 'Mike Dao', district: 7)
       visit '/'
 
       fill_in :street, with: '1271 S Marshall St'
       fill_in :city, with: 'Lakewood'
       fill_in :state, with: 'CO'
-      fill_in :zip, with: '80232'
-      click 'Submit'
+      fill_in :postal_code, with: '80232'
+      click_on 'Submit'
 
       expect(current_path).to eq '/representatives/7'
     end
