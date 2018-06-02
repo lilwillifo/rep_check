6323411431ff95309d78aea4a10399ff28f4fea9
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-02 10:32:35 -0600

    adding users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531203203) do
+ActiveRecord::Schema.define(version: 20180602163053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 20180531203203) do
     t.string "website"
     t.string "bioguide_id"
     t.integer "party", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "screen_name"
+    t.string "uid"
+    t.string "oauth_token"
+    t.string "oauth_token_secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bills", "categories"
