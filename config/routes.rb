2eba9ffb39eb3f24c9d8ff034eb05a870484a14f
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-02 10:25:07 -0600

    adding link to sign in with twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/auth/twitter', as: :twitter_login
+
+  get '/auth/twitter/callback', to: 'sessions#create'
 
   resources :representatives, only: [:index, :show]
-  resources :senators, only: [:index]
+  # resources :senators, only: [:index]
 end
