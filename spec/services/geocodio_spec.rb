92229368ece9a3a7c9d3310cc7ac65ca50f75cf9
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-02 14:36:08 -0600

    setting up geocodio service

--- spec/services/geocodio_spec.rb
+++ spec/services/geocodio_spec.rb
@@ -5,7 +5,11 @@ describe GeocodioService do
     describe "district" do
       it "returns district for a given address" do
         VCR.use_cassette("find_district") do
-          address = '1271 S Marshall St Denver, CO 80232'
+          address = { street: '1271 S Marshall St',
+                      city: 'Denver',
+                      state: 'CO',
+                      postal_code: '80232'
+                    }
 
           expect(GeocodioService.new(address).district).to eq(7)
         end
