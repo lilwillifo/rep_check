dc0174fdf4cea1fa027714ac011b04fc875da7d6
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-05 20:34:45 -0600

    month helper for view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def months
+    {
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+  }
+  end
+  def integer_to_month(int)
+    months[int]
+  end
 end
